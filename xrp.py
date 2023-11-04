90f2a3c63156b74988bf965ef18de4c263a1a8bc
Siyuan Fan <sf316@duke.edu>
2023-11-04 14:30:57 -0400

    Test XRP SDK

--- xrp.py
+++ xrp.py
@@ -11,3 +11,20 @@ if __name__ == "__main__":
 
     test_wallet = generate_faucet_wallet(client, debug=True)
     print(test_wallet)
+
+    # Create an account str from the wallet
+    test_account = test_wallet.address
+
+    # Look up info about your account
+    from xrpl.models.requests.account_info import AccountInfo
+    acct_info = AccountInfo(
+        account=test_account,
+        ledger_index="validated",
+        strict=True,
+    )
+    response = client.request(acct_info)
+    result = response.result
+    print("response.status: ", response.status)
+    
+    import json
+    print(json.dumps(response.result, indent=4, sort_keys=True))
